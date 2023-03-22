f8fab02bd0f2436b086a857fea6c1e92b466c153
Uliana <uosintseva@gmail.com>
2023-03-22 20:14:44 +0300

    change files

--- src/test/java/ru/netology/AfishaManagerTest.java
+++ src/test/java/ru/netology/AfishaManagerTest.java
@@ -13,8 +13,14 @@ public class AfishaManagerTest {
         manager.add("Film third");
         manager.add("Film fourth");
         manager.add("Film fifth");
+        manager.add("Film sixth");
+        manager.add("Film seventh");
+        manager.add("Film eighth");
+        manager.add("Film  ninth");
+        manager.add("Film tenth");
 
-        String[] expected = {"Film one", "Film second", "Film third", "Film fourth","Film fifth"};
+        String[] expected = {"Film one", "Film second", "Film third", "Film fourth", "Film fifth",
+                "Film sixth", "Film seventh", "Film eighth", "Film  ninth", "Film tenth"};
         String[] actual = manager.findAllMovie();
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -29,25 +35,52 @@ public class AfishaManagerTest {
     }
 
     @Test
-    public void testOne() {
+    public void testUnderLimit() {
         AfishaManager manager = new AfishaManager();
         manager.add("Film one");
         String[] expected = {"Film one"};
         String[] actual = manager.findAllMovie();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void findLast(){
+    public void findLast() {
         AfishaManager manager = new AfishaManager();
         manager.add("Film one");
         manager.add("Film second");
         manager.add("Film third");
         manager.add("Film fourth");
         manager.add("Film fifth");
+        manager.add("Film sixth");
+        manager.add("Film seventh");
+        manager.add("Film eighth");
+        manager.add("Film  ninth");
+        manager.add("Film tenth");
 
-        String[] expected = {"Film fifth","Film fourth","Film third","Film second","Film one"};
+        String[] expected = {"Film tenth", "Film  ninth", "Film eighth", "Film seventh", "Film sixth", "Film fifth",
+                "Film fourth", "Film third", "Film second", "Film one"};
         String[] actual = manager.findLastMovie();
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void findAboveLimit() {
+        AfishaManager manager = new AfishaManager();
+        manager.add("Film one");
+        manager.add("Film second");
+        manager.add("Film third");
+        manager.add("Film fourth");
+        manager.add("Film fifth");
+        manager.add("Film sixth");
+        manager.add("Film seventh");
+        manager.add("Film eighth");
+        manager.add("Film  ninth");
+        manager.add("Film tenth");
+        manager.add("Film eleventh");
+        String[] expected = {"Film one", "Film second", "Film third", "Film fourth", "Film fifth",
+                "Film sixth", "Film seventh", "Film eighth", "Film  ninth", "Film tenth","Film eleventh"};
+        String[] actual = manager.findAllMovie();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
